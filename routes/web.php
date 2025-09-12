47552593c37a3f2f41875e4b72ff04943e112f6e
Deep273 <soul42460@gmail.com>
2025-09-12 15:38:35 +0700

    incomplete_addition_of_reader_and_book_functions

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,9 @@ Route::add('GET', '/hello', [Controller\Site::class, 'hello'])
 Route::add(['GET', 'POST'], '/signup', [Controller\Site::class, 'signup']);
 Route::add(['GET', 'POST'], '/login', [Controller\Site::class, 'login']);
 Route::add('GET', '/logout', [Controller\Site::class, 'logout']);
+// Маршруты для книги
+Route::add('GET', '/create-book', [Controller\Site::class, 'createBook']);    // Форма добавления книги
+Route::add('POST', '/create-book', [Controller\Site::class, 'storeBook']);     // Обработка добавления книги
+Route::add('GET', '/books', [Controller\Site::class, 'listBooks']);            // Список всех книг
+Route::add('GET', '/create-reader', [Controller\Site::class, 'createReader']);  // Форма
+Route::add('POST', '/create-reader', [Controller\Site::class, 'storeReader']);  // Обработка
